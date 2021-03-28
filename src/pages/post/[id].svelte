fe7b2a385d6741a3672b262ccd48f88143b1aa5a
Hakan Alpay <hak7alp@gmail.com>
2021-03-28 11:16:55 -0700

    Update [id].svelte

--- src/pages/post/[id].svelte
+++ src/pages/post/[id].svelte
@@ -1,41 +1,30 @@
 <script>
-  import { goto, url } from "@roxi/routify";
+  import { goto } from "@roxi/routify";
   export let id;
 
-  import { Doc } from "sveltefire";
+  import { Doc, Collection } from "sveltefire";
 
-  const onImageClick = (e) => {
-    e.target.style.maxWidth = "200%";
-    e.target.style.maxHeight = "calc(200vh - 2rem - 2px)";
-  };
-
-  let theImage;
   let zoom = false;
 </script>
 
 <Doc path={`posts/${id}`} log let:data={post} let:ref={postRef} on:data on:ref>
   <div class="post">
     {#if post.imgURL}
-      <div
-        class="img-box"
-        style="background-image: url({post.imgURL})"
-        class:zoom
-        on:click={() => (zoom = !zoom)}
-      >
-        <!-- <img
+      {#if !zoom}
+        <div
+          class="img-box"
+          style="background-image: url({post.imgURL})"
+          on:click={() => (zoom = !zoom)}
+        />
+      {:else}
+        <img
+          class="zoom-img"
           src={post.imgURL}
+          on:click={() => (zoom = !zoom)}
           alt="post"
-          class="post-image"
-          on:click={(e) =>
-            e.target == document.fullscreenElement
-              ? document.exitFullscreen()
-              : e.target.requestFullscreen()}
-          bind:this={theImage}
-        /> -->
-      </div>
+        />
+      {/if}
     {/if}
-
-    <a href={post.imgURL}>Original Image</a>
 
     <div class="post-creator">
       <img src={post.creatorPhoto} alt="poster's profile" />
@@ -50,6 +39,14 @@
         $goto("/");
       }}>Delete</button
     >
+
+    <Collection
+      path={`posts/${id}/comments`}
+      query={(ref) => ref.orderBy("createdAt", "desc")}
+      let:data={comments}
+      let:ref={commentsRef}
+      log
+    />
   </div>
 
   <span slot="loading">Loading...</span>
@@ -57,53 +54,19 @@
 </Doc>
 
 <style>
-  .zoom {
-    transform-origin: center top;
-    /* transform: scale(2); */
-    height: 200vh !important;
+  .zoom-img {
+    width: 100%;
     cursor: zoom-out !important;
   }
   .img-box {
     max-width: 100%;
     width: auto;
-    height: calc(100vh - 1rem);
-    /* display: flex;
-    align-items: center; */
-    /* background: var(--img-url); */
+    height: calc(100vh - 2rem);
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center center;
 
     transition: transform 0.25s ease;
     cursor: zoom-in;
-  }
-  .post-image {
-    max-height: 100%;
-    max-width: inherit;
-    /* max-width: 100%;
-    max-height: 100%; */
-    /* width: 100%;
-    height: auto;
-    width: auto;
-    max-height: calc(100vh - 1rem); */
-    /* object-fit: cover; */
-    /* height: 100%; */
-    /* max-width: 100%;
-    width: auto;
-    max-height: calc(100vh - 1rem); */
-    /* 
-    
-    max-width: 10%;
-    max-height: 10%;
-    transform: scale(10); */
-
-    display: block;
-    cursor: pointer;
-    margin: auto;
-
-    /* left: 50%;
-    margin-left: -50vw;
-    margin-right: -50vw;
-    right: 50%;*/
   }
 </style>
